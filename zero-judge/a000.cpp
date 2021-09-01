689d7e63c02615c5852de52419306a4d738827a9
chin_chia <chinchia120@gmail.com>
2021-09-01 23:20:38 +0800

    update

--- zero-judge/a000.cpp
+++ zero-judge/a000.cpp
@@ -3,11 +3,5 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    int arr1[n],arr2[n];
-    for(int i=0;i<n;i++){
-        int _num;
-        cin>>_num;
-        arr1[i]=_num;
-        arr2[i]=_num;
-    }
+    cout<<n<<endl;
 }
