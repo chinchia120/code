b80e9bef536e2645e82ebd99c2491fe1e75d031f
chin_chia <chinchia120@gmail.com>
2021-09-26 21:53:47 +0800

    update

--- zero-judge/a000.cpp
+++ zero-judge/a000.cpp
@@ -1,19 +1,5 @@
 #include <iostream>
 using namespace std;
-int digit(int n){
-    int cnt=0;
-    while(n!=0){
-        n/=10;
-        cnt++;
-    }
-    return cnt;
-}
 int main(void){
-    int n;
-    while(cin>>n){
-        if(n==0){
-            return 0;
-        }
-        cout<<digit(n)<<endl;
-    }
+    
 }
