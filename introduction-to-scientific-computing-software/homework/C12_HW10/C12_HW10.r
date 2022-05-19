3380ac7bebfdfd73ab9127fe1dc7d5c324f4a8a0
chin_chia <chinchia120@gmail.com>
2022-05-19 15:55:48 +0800

    update

--- introduction-to-scientific-computing-software/homework/C12_HW10/C12_HW10.r
+++ introduction-to-scientific-computing-software/homework/C12_HW10/C12_HW10.r
@@ -4,7 +4,6 @@ getwd()
 #Q1
 data1 <- read.csv("Titanic.csv")
 
-library("ResourceSelection")
-mod_null <- glm(Class ~ 1, family = "binomial", data = data1)
-mod_full <- glm(Class ~ as.factor(Class) + as.factor(Sex) + as.factor(Age) + as.factor(Survived), family = "binomial", data = data1)
+mod_null <- glm(as.factor(Survived) ~ 1, family = "binomial", data = data1)
+mod_full <- glm(as.factor(Survived) ~ as.factor(Class) + as.factor(Sex) + as.factor(Age), family = "binomial", data = data1)
 step(mod_null, scope = list(lower = mod_null, upper = mod_full), direction = "forward", trace = 1)
