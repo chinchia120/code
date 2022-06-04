515c7aa57a890596e6da2d5efecc43f72fe77338
chin_chia <chinchia120@gmail.com>
2022-06-04 17:07:34 +0800

    update

--- introduction-to-scientific-computing-software/report/final_report/final_report.r
+++ introduction-to-scientific-computing-software/report/final_report/final_report.r
@@ -7,14 +7,14 @@ data_stationary <- read.csv("平均每戶書報雜誌文具支出占消費支出比率.csv")
 data_area <- read.csv("平均每人居住面積(坪).csv")
 data_education <- read.csv("15歲以上民間人口之教育程度結構-大專及以上(％).csv")
 
-View(data_outcome)
-View(data_income)
-View(data_stationary)
-View(data_area)
-View(data_education)
+#View(data_outcome)
+#View(data_income)
+#View(data_stationary)
+#View(data_area)
+#View(data_education)
 
 #ANOVA_Q2
-cor.test(as.numeric(data_outcome$臺北市), as.numeric(data_stationary$臺北市) , method = "spearman")
+cor.test(data_outcome$臺北市, data_stationary$臺北市 , method = "spearman")
 
 #連續型
 
