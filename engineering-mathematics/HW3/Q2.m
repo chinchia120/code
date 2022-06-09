ba0400dbc43d983d254a2e30408addb280103c2e
chin_chia <chinchia120@gmail.com>
2022-06-10 00:40:33 +0800

    update

--- engineering-mathematics/HW3/Q2.m
+++ engineering-mathematics/HW3/Q2.m
@@ -3,78 +3,23 @@ clf;
 clear;
 
 syms x;
-fx = x*(x-pi())*(x-2*pi());
+fx = x*(x-2)*(x-4);
 left = 2;
 size = 0.001;
 right = 4;
 width = right - left;
 
-%T = 1
-num_period = 1;
-t = [left:size:right-size];
-z = subs(fx,x,t);
-array0 = [];
+for i = 1:1:5
+    num_period = 2*i-1;
+    t = [left:size:right-size];
+    z = subs(fx,x,t);
+    array0 = [];
 
-for i = 1:num_period
-    array0 = [array0 z];
+    for j = 1:num_period
+        array0 = [array0 z];
+    end
+
+    t = [left-(floor(num_period/2))*width:size:right+(floor(num_period/2))*width-size];
+    subplot(5,1,i);
+    plot(t,array0);
 end
-
-t = [left-(floor(num_period/2))*width:size:right+(floor(num_period/2))*width-size];
-subplot(5,1,1);
-plot(t,array0);
-
-%T = 3
-num_period = 3;
-t = [left:size:right-size];
-z = subs(fx,x,t);
-array0 = [];
-
-for i = 1:num_period
-    array0 = [array0 z];
-end
-
-t = [left-(floor(num_period/2))*width:size:right+(floor(num_period/2))*width-size];
-subplot(5,1,2);
-plot(t,array0);
-
-%T = 5
-num_period = 5;
-t = [left:size:right-size];
-z = subs(fx,x,t);
-array0 = [];
-
-for i = 1:num_period
-    array0 = [array0 z];
-end
-
-t = [left-(floor(num_period/2))*width:size:right+(floor(num_period/2))*width-size];
-subplot(5,1,3);
-plot(t,array0);
-
-%T = 7
-num_period = 7;
-t = [left:size:right-size];
-z = subs(fx,x,t);
-array0 = [];
-
-for i = 1:num_period
-    array0 = [array0 z];
-end
-
-t = [left-(floor(num_period/2))*width:size:right+(floor(num_period/2))*width-size];
-subplot(5,1,4);
-plot(t,array0);
-
-%T = 9
-num_period = 9;
-t = [left:size:right-size];
-z = subs(fx,x,t);
-array0 = [];
-
-for i = 1:num_period
-    array0 = [array0 z];
-end
-
-t = [left-(floor(num_period/2))*width:size:right+(floor(num_period/2))*width-size];
-subplot(5,1,5);
-plot(t,array0);
