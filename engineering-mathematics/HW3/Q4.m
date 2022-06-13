92f93d396702d5961a9fef249daea5b595ebfbbd
chin_chia <chinchia120@gmail.com>
2022-06-13 18:03:25 +0800

    update

--- engineering-mathematics/HW3/Q4.m
+++ engineering-mathematics/HW3/Q4.m
@@ -6,9 +6,9 @@ pi_ = pi();
 syms x n pi;
 fx = x^2;
 a0 = 1/(2*pi)*int(fx,x,-pi,pi);
-an = 1/(2*pi)*int(fx*cos(n*x),x,-pi,pi);
-bn = 1/(2*pi)*int(fx*sin(n*x),x,-pi,pi);
+an = 1/(pi)*int(fx*cos(n*x),x,-pi,pi)
+bn = 1/(pi)*int(fx*sin(n*x),x,-pi,pi);
 
 for i = 1:10
-    subs(an,[n,pi],[i,pi_]);
+    subs(an,[n,pi],[i,pi_])
 end    
