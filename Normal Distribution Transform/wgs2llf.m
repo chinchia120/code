990e5adc0e8d66b3a537b98eb25672e6b4c7cccc
chinchia120 <71184995+chinchia120@users.noreply.github.com>
2023-12-05 22:17:06 +0800

    Delete Normal Distribution Transform directory

--- Normal Distribution Transform/wgs2llf.m
+++
@@ -1,9 +0,0 @@
-%% =============== Function of wgs2llf =============== %%
-function [LLF_NED_GT0] = wgs2llf(f_ref,initPOS)
-    wgs84 = wgs84Ellipsoid;
-    [N, E, D] = geodetic2ned(f_ref(:, 2), f_ref(:, 3), f_ref(:, 4), initPOS(1), initPOS(2), initPOS(3), wgs84);
-    LLF_NED_GT0 = f_ref;
-    LLF_NED_GT0(:, 2) = N;
-    LLF_NED_GT0(:, 3) = E;
-    LLF_NED_GT0(:, 4) = D;
-end
